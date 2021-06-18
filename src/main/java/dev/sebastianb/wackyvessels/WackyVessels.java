395a9058cacd44a01547d11e6ed9fb7be7918dcf
SebaSphere <SebaSphereEmails@gmail.com>
2021-06-17 21:28:32 -0400

    Initial commit

---
+++ src/main/java/dev/sebastianb/wackyvessels/WackyVessels.java
@@ -0,0 +1,12 @@
+package dev.sebastianb.wackyvessels;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WackyVessels implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+    
+}
---
+++ src/main/java/dev/sebastianb/wackyvessels/client/WackyVesselsClient.java
@@ -0,0 +1,10 @@
+package dev.sebastianb.wackyvessels.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WackyVesselsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
